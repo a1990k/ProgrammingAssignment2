d07d1087bc628b09c8c8c049b079abce82cb71ed
a1990k <a1990k@gmail.com>
2014-12-21 23:03:47 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##takes a matrix myX and caches it in X
+#also creates the Xinv variable which holds the inverse when needed
 makeCacheMatrix <- function(myX = matrix()) {
         Xinv <<- NULL
-        X <- myX
+        X <<- myX
 }
 
 
-## Write a short comment describing this function
-
+##takes a matrix myX and return its inverse
 cacheSolve <- function(myX, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        if(!is.null(Xinv)&&(myX==X)) {
-                message("getting matrix inverse")
+#if the inverse haven't been calculated yet or the the matrix myX has changed from X
+#we cache the matrix myX compute the inverse and cache it in Xinv then we return it
+        if((is.null(Xinv))||(!identical(X,myX))) {
+                X <<- myX
+                Xinv <<- solve(X)
                 return(Xinv)
         }
-        X <<- myX
-        Xinv <<- solve(X)
+#if the inverse has been already computed and the matrix myX hasn't 
+#changed from X, we return the cached inverse Xinv
+        message("getting matrix inverse")
         Xinv
 }
